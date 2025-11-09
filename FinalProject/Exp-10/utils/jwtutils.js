a10c87372193e7d10794df797f693964bd887376
Sayan Pradhan <sayan.2020.300@gmail.com>
2025-11-09 23:19:58 +0530

    Initial commit - full project folder

--- FinalProject/Exp-10/utils/jwtutils.js
+++
@@ -1,9 +0,0 @@
-const jwt = require("jsonwebtoken");
-const secretKey = "yourSecretKey";
-
-// Generate a JWT with user role embedded in payload
-function generateToken(user) {
-  return jwt.sign(user, secretKey, { expiresIn: "1h" });
-}
-
-module.exports = { generateToken, secretKey };
